b5c0c4d0198c642957d4ebd242ab4531f3b78cbc
Mark Nelson <mnel0001@MacBook-Pro-2.local>
2019-11-21 15:44:20 +0000

    updated tokens

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseAPIKey: 'AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y'
+  firebaseAPIKey: 'AIzaSyBbA5PNqnGtpuhQaKQiIRZGCrm2A6s9UY0'
 };
 
 /*
